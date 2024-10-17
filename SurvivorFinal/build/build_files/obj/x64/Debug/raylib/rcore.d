8bb0699b295627afc288742cdca07410196c92b2
Justin Gonzalez <justin.gonzalez016@mymdc.net>
2024-10-17 02:05:34 +0000

    MORE

---
+++ SurvivorFinal/build/build_files/obj/x64/Debug/raylib/rcore.d
@@ -0,0 +1,125 @@
+obj/x64/Debug/raylib/rcore.o: ../external/raylib-master/src/rcore.c \
+ ../external/raylib-master/src/raylib.h \
+ C:/raylib/w64devkit/lib/gcc/x86_64-w64-mingw32/13.2.0/include/stdarg.h \
+ C:/raylib/w64devkit/x86_64-w64-mingw32/include/stdarg.h \
+ C:/raylib/w64devkit/x86_64-w64-mingw32/include/_mingw_stdarg.h \
+ C:/raylib/w64devkit/x86_64-w64-mingw32/include/vadefs.h \
+ C:/raylib/w64devkit/x86_64-w64-mingw32/include/_mingw.h \
+ C:/raylib/w64devkit/x86_64-w64-mingw32/include/_mingw_mac.h \
+ C:/raylib/w64devkit/x86_64-w64-mingw32/include/_mingw_secapi.h \
+ C:/raylib/w64devkit/x86_64-w64-mingw32/include/sdks/_mingw_ddk.h \
+ C:/raylib/w64devkit/lib/gcc/x86_64-w64-mingw32/13.2.0/include/stdbool.h \
+ ../external/raylib-master/src/config.h \
+ ../external/raylib-master/src/utils.h \
+ C:/raylib/w64devkit/x86_64-w64-mingw32/include/stdlib.h \
+ C:/raylib/w64devkit/x86_64-w64-mingw32/include/corecrt.h \
+ C:/raylib/w64devkit/x86_64-w64-mingw32/include/corecrt_wstdlib.h \
+ C:/raylib/w64devkit/lib/gcc/x86_64-w64-mingw32/13.2.0/include/limits.h \
+ C:/raylib/w64devkit/lib/gcc/x86_64-w64-mingw32/13.2.0/include/syslimits.h \
+ C:/raylib/w64devkit/x86_64-w64-mingw32/include/limits.h \
+ C:/raylib/w64devkit/x86_64-w64-mingw32/include/crtdefs.h \
+ C:/raylib/w64devkit/x86_64-w64-mingw32/include/sec_api/stdlib_s.h \
+ C:/raylib/w64devkit/x86_64-w64-mingw32/include/malloc.h \
+ C:/raylib/w64devkit/lib/gcc/x86_64-w64-mingw32/13.2.0/include/mm_malloc.h \
+ C:/raylib/w64devkit/x86_64-w64-mingw32/include/errno.h \
+ C:/raylib/w64devkit/x86_64-w64-mingw32/include/stdio.h \
+ C:/raylib/w64devkit/x86_64-w64-mingw32/include/corecrt_stdio_config.h \
+ C:/raylib/w64devkit/x86_64-w64-mingw32/include/_mingw_off_t.h \
+ C:/raylib/w64devkit/x86_64-w64-mingw32/include/swprintf.inl \
+ C:/raylib/w64devkit/x86_64-w64-mingw32/include/sec_api/stdio_s.h \
+ C:/raylib/w64devkit/x86_64-w64-mingw32/include/string.h \
+ C:/raylib/w64devkit/x86_64-w64-mingw32/include/sec_api/string_s.h \
+ C:/raylib/w64devkit/x86_64-w64-mingw32/include/time.h \
+ C:/raylib/w64devkit/x86_64-w64-mingw32/include/sys/timeb.h \
+ C:/raylib/w64devkit/x86_64-w64-mingw32/include/sec_api/sys/timeb_s.h \
+ C:/raylib/w64devkit/x86_64-w64-mingw32/include/_timeval.h \
+ C:/raylib/w64devkit/x86_64-w64-mingw32/include/pthread_time.h \
+ C:/raylib/w64devkit/x86_64-w64-mingw32/include/math.h \
+ ../external/raylib-master/src/rlgl.h \
+ ../external/raylib-master/src/external/glad.h \
+ C:/raylib/w64devkit/lib/gcc/x86_64-w64-mingw32/13.2.0/include/stdint.h \
+ C:/raylib/w64devkit/x86_64-w64-mingw32/include/stdint.h \
+ C:/raylib/w64devkit/lib/gcc/x86_64-w64-mingw32/13.2.0/include/stddef.h \
+ C:/raylib/w64devkit/x86_64-w64-mingw32/include/stddef.h \
+ ../external/raylib-master/src/raymath.h \
+ ../external/raylib-master/src/rgestures.h \
+ ../external/raylib-master/src/rcamera.h \
+ ../external/raylib-master/src/external/msf_gif.h \
+ C:/raylib/w64devkit/lib/gcc/x86_64-w64-mingw32/13.2.0/include/emmintrin.h \
+ C:/raylib/w64devkit/lib/gcc/x86_64-w64-mingw32/13.2.0/include/xmmintrin.h \
+ C:/raylib/w64devkit/lib/gcc/x86_64-w64-mingw32/13.2.0/include/mmintrin.h \
+ ../external/raylib-master/src/external/sinfl.h \
+ C:/raylib/w64devkit/x86_64-w64-mingw32/include/assert.h \
+ ../external/raylib-master/src/external/sdefl.h \
+ ../external/raylib-master/src/external/rprand.h \
+ C:/raylib/w64devkit/x86_64-w64-mingw32/include/sys/stat.h \
+ C:/raylib/w64devkit/x86_64-w64-mingw32/include/io.h \
+ C:/raylib/w64devkit/x86_64-w64-mingw32/include/sys/types.h \
+ C:/raylib/w64devkit/x86_64-w64-mingw32/include/_mingw_stat64.h \
+ C:/raylib/w64devkit/x86_64-w64-mingw32/include/dirent.h \
+ C:/raylib/w64devkit/x86_64-w64-mingw32/include/direct.h \
+ ../external/raylib-master/src/platforms/rcore_desktop_glfw.c \
+ ../external/raylib-master/src/external/glfw/include/GLFW/glfw3.h \
+ ../external/raylib-master/src/external/glfw/include/GLFW/glfw3native.h
+../external/raylib-master/src/raylib.h:
+C:/raylib/w64devkit/lib/gcc/x86_64-w64-mingw32/13.2.0/include/stdarg.h:
+C:/raylib/w64devkit/x86_64-w64-mingw32/include/stdarg.h:
+C:/raylib/w64devkit/x86_64-w64-mingw32/include/_mingw_stdarg.h:
+C:/raylib/w64devkit/x86_64-w64-mingw32/include/vadefs.h:
+C:/raylib/w64devkit/x86_64-w64-mingw32/include/_mingw.h:
+C:/raylib/w64devkit/x86_64-w64-mingw32/include/_mingw_mac.h:
+C:/raylib/w64devkit/x86_64-w64-mingw32/include/_mingw_secapi.h:
+C:/raylib/w64devkit/x86_64-w64-mingw32/include/sdks/_mingw_ddk.h:
+C:/raylib/w64devkit/lib/gcc/x86_64-w64-mingw32/13.2.0/include/stdbool.h:
+../external/raylib-master/src/config.h:
+../external/raylib-master/src/utils.h:
+C:/raylib/w64devkit/x86_64-w64-mingw32/include/stdlib.h:
+C:/raylib/w64devkit/x86_64-w64-mingw32/include/corecrt.h:
+C:/raylib/w64devkit/x86_64-w64-mingw32/include/corecrt_wstdlib.h:
+C:/raylib/w64devkit/lib/gcc/x86_64-w64-mingw32/13.2.0/include/limits.h:
+C:/raylib/w64devkit/lib/gcc/x86_64-w64-mingw32/13.2.0/include/syslimits.h:
+C:/raylib/w64devkit/x86_64-w64-mingw32/include/limits.h:
+C:/raylib/w64devkit/x86_64-w64-mingw32/include/crtdefs.h:
+C:/raylib/w64devkit/x86_64-w64-mingw32/include/sec_api/stdlib_s.h:
+C:/raylib/w64devkit/x86_64-w64-mingw32/include/malloc.h:
+C:/raylib/w64devkit/lib/gcc/x86_64-w64-mingw32/13.2.0/include/mm_malloc.h:
+C:/raylib/w64devkit/x86_64-w64-mingw32/include/errno.h:
+C:/raylib/w64devkit/x86_64-w64-mingw32/include/stdio.h:
+C:/raylib/w64devkit/x86_64-w64-mingw32/include/corecrt_stdio_config.h:
+C:/raylib/w64devkit/x86_64-w64-mingw32/include/_mingw_off_t.h:
+C:/raylib/w64devkit/x86_64-w64-mingw32/include/swprintf.inl:
+C:/raylib/w64devkit/x86_64-w64-mingw32/include/sec_api/stdio_s.h:
+C:/raylib/w64devkit/x86_64-w64-mingw32/include/string.h:
+C:/raylib/w64devkit/x86_64-w64-mingw32/include/sec_api/string_s.h:
+C:/raylib/w64devkit/x86_64-w64-mingw32/include/time.h:
+C:/raylib/w64devkit/x86_64-w64-mingw32/include/sys/timeb.h:
+C:/raylib/w64devkit/x86_64-w64-mingw32/include/sec_api/sys/timeb_s.h:
+C:/raylib/w64devkit/x86_64-w64-mingw32/include/_timeval.h:
+C:/raylib/w64devkit/x86_64-w64-mingw32/include/pthread_time.h:
+C:/raylib/w64devkit/x86_64-w64-mingw32/include/math.h:
+../external/raylib-master/src/rlgl.h:
+../external/raylib-master/src/external/glad.h:
+C:/raylib/w64devkit/lib/gcc/x86_64-w64-mingw32/13.2.0/include/stdint.h:
+C:/raylib/w64devkit/x86_64-w64-mingw32/include/stdint.h:
+C:/raylib/w64devkit/lib/gcc/x86_64-w64-mingw32/13.2.0/include/stddef.h:
+C:/raylib/w64devkit/x86_64-w64-mingw32/include/stddef.h:
+../external/raylib-master/src/raymath.h:
+../external/raylib-master/src/rgestures.h:
+../external/raylib-master/src/rcamera.h:
+../external/raylib-master/src/external/msf_gif.h:
+C:/raylib/w64devkit/lib/gcc/x86_64-w64-mingw32/13.2.0/include/emmintrin.h:
+C:/raylib/w64devkit/lib/gcc/x86_64-w64-mingw32/13.2.0/include/xmmintrin.h:
+C:/raylib/w64devkit/lib/gcc/x86_64-w64-mingw32/13.2.0/include/mmintrin.h:
+../external/raylib-master/src/external/sinfl.h:
+C:/raylib/w64devkit/x86_64-w64-mingw32/include/assert.h:
+../external/raylib-master/src/external/sdefl.h:
+../external/raylib-master/src/external/rprand.h:
+C:/raylib/w64devkit/x86_64-w64-mingw32/include/sys/stat.h:
+C:/raylib/w64devkit/x86_64-w64-mingw32/include/io.h:
+C:/raylib/w64devkit/x86_64-w64-mingw32/include/sys/types.h:
+C:/raylib/w64devkit/x86_64-w64-mingw32/include/_mingw_stat64.h:
+C:/raylib/w64devkit/x86_64-w64-mingw32/include/dirent.h:
+C:/raylib/w64devkit/x86_64-w64-mingw32/include/direct.h:
+../external/raylib-master/src/platforms/rcore_desktop_glfw.c:
+../external/raylib-master/src/external/glfw/include/GLFW/glfw3.h:
+../external/raylib-master/src/external/glfw/include/GLFW/glfw3native.h:
